fa0a553963fb2cd097ccbcc4773954a0d0c58c6e
Mall1o <t.mansi@studenti.unisa.it>
2024-08-30 13:51:33 +0200

    un poò di casotto sul frontend

--- frontend/src/_mock/account.js
+++
@@ -1,7 +0,0 @@
-// ----------------------------------------------------------------------
-
-export const account = {
-  displayName: 'Gianfranco Barba',
-  email: 'g.barba14@studenti.unisa.it',
-  photoURL: '/assets/images/avatars/avatar_25.jpg',
-};
