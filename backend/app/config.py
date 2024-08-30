388ea245e57d0c3d646b20bd72875a908c442105
Mall1o <t.mansi@studenti.unisa.it>
2024-08-30 16:52:47 +0200

    Nuovo frontend, speriamo definitivo

--- backend/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    NEO4J_URI = os.getenv('NEO4J_URI', 'bolt://localhost:7687')
-    NEO4J_USERNAME = os.getenv('NEO4J_USERNAME', 'neo4j')
-    NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD', 'password')
-    SECRET_KEY = os.getenv('SECRET_KEY', 'mysecret')
