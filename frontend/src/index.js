b16a98799f345a4914f065d4c6252906fc416e31
gianfrancobarba <gianfrancobarba25@gmail.com>
2024-09-07 18:50:56 +0200

    ancora imperfetta ma modificata la homepage, rimozione di tutta la merda vecchia, non mi piace il footer, la navbar trasparende la voglio rendere opaca così non si vede la roba della home, cambiare tutta la roba dei caratteri, selezioni ecc.. il footer manco mi piace cosi ma va perfezionato

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';  // Assicurati che BrowserRouter sia importato
 import App from './App';
 import './index.css';
-import ThemeProvider from './theme/index/ThemeProvider';  // Importa ThemeProvider
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <BrowserRouter>  {/* Avvolgi App con BrowserRouter */}
-        <ThemeProvider>
           <App />
-        </ThemeProvider>
       </BrowserRouter>
   </StrictMode>,
 );
