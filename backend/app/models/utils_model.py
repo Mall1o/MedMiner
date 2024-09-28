38b21119f3e5a5a2d6cc75f65d40c7475a5c6f22
gianfrancobarba <gianfrancobarba25@gmail.com>
2024-09-28 17:09:45 +0200

    update dump db

--- backend/app/models/utils_model.py
+++ backend/app/models/utils_model.py
@@ -266,70 +266,78 @@ class UtilsModel:
         file_path = file_path.replace("\\", "/")
         queries = [
             f"""
+            // Creazione Paziente
             LOAD CSV WITH HEADERS FROM 'file:///{file_name}' AS row
-            WITH DISTINCT row.codice_fiscale_assistito AS codice_fiscale_assistito,
-                row.eta_prima_diagnosi AS eta_prima_diagnosi, 
-                row.sesso AS sesso
+            WITH DISTINCT row.CODICE_FISCALE_ASSISTITO AS codice_fiscale_assistito,
+                row.ANNO_NASCITA AS anno_nascita, 
+                row.SESSO AS sesso, 
+                row.CAP_RESIDENZA AS cap_residenza
             WHERE codice_fiscale_assistito IS NOT NULL
             MERGE (p:Paziente {{codice_fiscale_assistito: codice_fiscale_assistito}})
-            ON CREATE SET p.eta_prima_diagnosi = toFloat(eta_prima_diagnosi),
-                        p.sesso = sesso
+            ON CREATE SET p.anno_nascita = toInteger(anno_nascita),
+                        p.sesso = sesso,
+                        p.cap_residenza = toInteger(cap_residenza)
             """,
 
             f"""
+            // Creazione Prescrizione
             LOAD CSV WITH HEADERS FROM 'file:///{file_name}' AS row
-            WITH DISTINCT row.codice_prescrizione AS codice_prescrizione
+            WITH DISTINCT row.CODICE_PRESCRIZIONE AS codice_prescrizione
             WHERE codice_prescrizione IS NOT NULL
             MERGE (pr:Prescrizione {{codice: codice_prescrizione}})
             """,
 
             f"""
+            // Creazione Malattia
             LOAD CSV WITH HEADERS FROM 'file:///{file_name}' AS row
-            WITH DISTINCT row.icd9_cm AS codice_malattia, row.descrizione_malattia AS descrizione_malattia
+            WITH DISTINCT row.ICD9_CM AS codice_malattia, row.DESCRIZIONE_MALATTIA AS descrizione_malattia
             WHERE codice_malattia IS NOT NULL
             MERGE (m:Malattia {{codice: codice_malattia}})
-            ON CREATE SET m.descrizione = descrizione_malattia
+            ON CREATE SET m.descrizione_malattia = descrizione_malattia
             """,
 
             f"""
+            // Creazione relazione DIAGNOSTICATO_CON tra Paziente e Malattia
             LOAD CSV WITH HEADERS FROM 'file:///{file_name}' AS row
-            WITH row.codice_fiscale_assistito AS codice_fiscale_assistito,
-                row.icd9_cm AS codice_malattia, 
-                row.data_prescrizione AS data_prescrizione,
-                row.data_prima_diagnosi AS data_prima_diagnosi,
-                row.data_ultima_diagnosi AS data_ultima_diagnosi
+            WITH row.CODICE_FISCALE_ASSISTITO AS codice_fiscale_assistito,
+                row.ICD9_CM AS codice_malattia, 
+                row.DATA_PRESCRIZIONE AS data_prescrizione,
+                row.DATA_PRIMA_DIAGNOSI AS data_prima_diagnosi,
+                row.DATA_ULTIMA_DIAGNOSI AS data_ultima_diagnosi
             WHERE codice_fiscale_assistito IS NOT NULL AND codice_malattia IS NOT NULL
-            MERGE (p:Paziente {{codice_fiscale: codice_fiscale_assistito}})
+            MERGE (p:Paziente {{codice_fiscale_assistito: codice_fiscale_assistito}})
             MERGE (m:Malattia {{codice: codice_malattia}})
             MERGE (p)-[r:DIAGNOSTICATO_CON]->(m)
             ON CREATE SET r.num_diagnosi = 1,
                         r.data_prima_diagnosi = data_prima_diagnosi,
                         r.data_ultima_diagnosi = data_ultima_diagnosi
             ON MATCH SET r.data_prima_diagnosi = CASE 
-                                                WHEN r.data_prima_diagnosi > data_prescrizione THEN data_prescrizione 
-                                                ELSE r.data_prima_diagnosi 
+                                                    WHEN r.data_prima_diagnosi > data_prescrizione THEN data_prescrizione 
+                                                    ELSE r.data_prima_diagnosi 
                                                 END,
                         r.data_ultima_diagnosi = CASE 
                                                     WHEN r.data_ultima_diagnosi < data_prescrizione THEN data_prescrizione 
                                                     ELSE r.data_ultima_diagnosi 
                                                 END,
                         r.num_diagnosi = r.num_diagnosi + CASE 
-                                                            WHEN r.data_prima_diagnosi <> data_prescrizione AND r.data_ultima_diagnosi <> data_prescrizione THEN 1 
+                                                            WHEN r.data_prima_diagnosi <> data_prescrizione 
+                                                            AND r.data_ultima_diagnosi <> data_prescrizione THEN 1 
                                                             ELSE 0 
-                                                        END
+                                                            END
             """,
 
             f"""
+            // Creazione relazione CURATA_CON tra Malattia e Prescrizione
             LOAD CSV WITH HEADERS FROM 'file:///{file_name}' AS row
-            WITH row.icd9_cm AS codice_malattia, 
-                row.codice_prescrizione AS codice_prescrizione,
-                row.descrizione_prescrizione AS descrizione_prescrizione,
-                row.codice_regionale_medico AS codice_regionale_medico,
-                row.data_prescrizione AS data_prescrizione,
-                row.tipo_prescrizione AS tipo_prescrizione,
-                row.quantita_prescrizione AS quantita_prescrizione,
-                row.codice_fiscale_assistito AS codice_fiscale_assistito,
-                row.eta AS eta_al_momento
+            WITH row.ICD9_CM AS codice_malattia, 
+                row.CODICE_PRESCRIZIONE AS codice_prescrizione,
+                row.DESCRIZIONE_PRESCRIZIONE AS descrizione_prescrizione,
+                row.CODICE_REGIONALE_MEDICO AS codice_regionale_medico,
+                row.DATA_PRESCRIZIONE AS data_prescrizione,
+                row.TIPO_PRESCRIZIONE AS tipo_prescrizione,
+                row.QUANTITA_PRESCRIZIONE AS quantita_prescrizione,
+                row.CODICE_FISCALE_ASSISTITO AS codice_fiscale_assistito,
+                row.ETA AS eta_al_momento
             WHERE codice_malattia IS NOT NULL AND codice_prescrizione IS NOT NULL
             MATCH (m:Malattia {{codice: codice_malattia}})
             MATCH (pr:Prescrizione {{codice: codice_prescrizione}})
@@ -341,6 +349,26 @@ class UtilsModel:
                 r.quantita_prescrizione = toFloat(quantita_prescrizione),
                 r.codice_fiscale_assistito = codice_fiscale_assistito,
                 r.eta_al_momento = toInteger(eta_al_momento)
+            """,
+
+            f"""
+            // Creazione relazione ASSOCIATA_A tra Malattie
+            LOAD CSV WITH HEADERS FROM 'file:///{file_name}' AS row
+            WITH row.CODICE_FISCALE_ASSISTITO AS codice_fiscale_assistito,
+                row.CODICE_PRESCRIZIONE AS codice_prescrizione,
+                row.ICD9_CM AS codice_malattia,
+                row.DATA_PRESCRIZIONE AS data_prescrizione
+            WHERE codice_fiscale_assistito IS NOT NULL AND codice_prescrizione IS NOT NULL AND codice_malattia IS NOT NULL
+            WITH codice_fiscale_assistito, codice_prescrizione, collect(DISTINCT codice_malattia) AS malattie, max(data_prescrizione) AS data_ultima_associazione
+            UNWIND range(0, size(malattie) - 2) AS i
+            UNWIND range(i + 1, size(malattie) - 1) AS j
+            WITH codice_fiscale_assistito, codice_prescrizione, malattie[i] AS malattia1, malattie[j] AS malattia2, data_ultima_associazione
+            MATCH (m1:Malattia {{codice: malattia1}}), (m2:Malattia {{codice: malattia2}})
+            MERGE (m1)-[r:ASSOCIATA_A {{codice_fiscale_assistito: codice_fiscale_assistito, codice_prescrizione: codice_prescrizione}}]->(m2)
+            ON CREATE SET r.count = 1,
+                        r.data_ultima_associazione = data_ultima_associazione
+            ON MATCH SET r.count = r.count + 1,
+                        r.data_ultima_associazione = data_ultima_associazione
             """
         ]
 
